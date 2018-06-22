8845b565cedb70fbcd5a9839e9c17cca7589a29c
oliverbudke <oliver.budke@campus.tu-berlin.de>
2018-06-22 09:41:02 +0200

    Presentation Update

--- dataExtractor.py
+++ dataExtractor.py
@@ -1,12 +1,11 @@
 import sys
 
-from wfdb import rdrecord, rdsamp
+from wfdb import rdsamp
 
 
 def write_data(input_file, output_file):
     signals, fields = rdsamp(input_file, sampfrom=0, sampto='end', channels=[2])
     print(signals)
-    print()
     print(fields)
 
 
